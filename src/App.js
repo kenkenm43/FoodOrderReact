ec3b82f50a1a322828311b77f1b5baa42053a7a5
kenkenm43 <kenkenm43@gmail.com>
2022-01-16 22:52:58 +0700

    ADD RouterDom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
